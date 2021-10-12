4246c1e12ec662bc5d81d9a688c2e50eb1093935
Joao Amaral <joaovillanova@hotmail.com.br>
2021-10-12 10:45:43 -0300

    Substituindo a função merge_columns pelo paradigma funcional - no for loops

--- dados_duplicados.R
+++ dados_duplicados.R
@@ -1,8 +1,4 @@
-library(dplyr)
-library(ggplot2)
-library(readr)
-library(tidyr)
-library(tibble)
+library(tidyverse)
 
 esus <- read_csv2("R/esus.csv", col_types = cols(.default = "c"))
 esus_confirmados <- read_csv2("R/confirmados.csv", col_types = cols(.default = "c"))
@@ -22,43 +18,17 @@ classificacao_ignorada <- esus_filtered %>%
   filter(classificação_final == "Não preenchido")
 
 
-# Substitui detectável/confirmado por TRUE e o oposto para FALSE
+# Substitui detectável/confirmado por TRUE e o oposto para FALSE e avalia se existe QUALQUER teste positivo
+  # em cada linha. Retorna para "testes_positivos" um vetor booleano
 testes_positivos <- classificacao_ignorada %>%
   select(contains("Resultado")) %>%
-  mutate(across(.cols = everything(), function(x) (x == "Detectável" | x == "Reagente"))) 
-
-#Junta as colunas dos testes em uma só, sendo TRUE se qualquer teste for positivo, ou FALSE caso nenhum 
-merge_bool_columns <- function(df) {
-  # Calcula numero de linhas da DF
-  len <- nrow(df)
-  # Um vetor de booleanos, que representará cada linha da df
-  bool_vec <- c() 
-  # Um vetor que irá representar a coluna do resultado final
-  col_vec <- c()
-  
-  # Uso o primeiro for para ir de 1 até o número total de linhas
-  for (num in 1:len) {
-    # "valor" representará cada valor de uma linha na DF
-    for (valor in df[num,]) {
-      bool_vec <- c(bool_vec, valor)
-    }
-    # Ao somar todos os valores booleanos no vetor bool_vec, saberemos se existe algum valor TRUE em cada linha da dataframe
-    ## Vale lembrar que TRUE = 1 e FALSE = 0. Dessa forma, qualquer soma > 0 corresponderá à TRUE
-    col_vec <- c(col_vec, (sum(bool_vec) > 0))
-    # Reseta o vetor bool_vec para estar vazio na próxima iteração do loop
-    bool_vec <- c()
-  }
-  
-  return(col_vec)
-}
-
-# Coluna com booleans que será usada para filtra a DF
-filter_col <- merge_bool_columns(testes_positivos)
+  mutate(across(.cols = everything(), function(x) (x == "Detectável" | x == "Reagente"))) %>%
+  transmute(resultado_final = map_lgl(pmap_dbl(.,sum), ~(.x>0)))
 
 # Filtra com base no vetor acima e classifica esses pacientes como confirmados
 confirmados_sem_classificação <- classificacao_ignorada %>%
   select(-contains("Resultado")) %>%
-  filter(filter_col) %>%
+  filter(testes_positivos) %>%
   mutate(classificação_final = "Confirmado Laboratorial")
 
 # Pega somente os casos confirmados, independente do tipo de confirmação
@@ -71,6 +41,8 @@ confirmados <- esus_filtered %>%
   rbind(confirmados_sem_classificação)
 
 ############ Testes com gráfico ####################
+# TODO
+# Não vai funcionar porque eu alterei o código acima
 first_col_values <- colnames(testes_positivos)
 second_col_values <- unname(sapply(testes_positivos, sum))
 
